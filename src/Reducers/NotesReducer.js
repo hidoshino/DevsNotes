5ef2b027c375785309c2e83a537a7f1d512f9fd3
hidoshino <83619908+hidoshino@users.noreply.github.com>
2021-09-18 15:49:40 -0300

    Config Redux

---
+++ src/Reducers/NotesReducer.js
@@ -0,0 +1,16 @@
+const initalState = {
+    list: [
+        {title: 'Tests', body: 'testando initalState reducer'}
+    ]
+}
+
+export default (state = initalState, action) => {
+
+    switch(action){
+        case  'ADD_NOTE': 
+        
+        break;
+    }
+
+    return state;
+}
